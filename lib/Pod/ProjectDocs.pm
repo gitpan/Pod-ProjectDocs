68a828fd4f7f23dae89e97596ad62bf2602fbea3
Lyo Kato <lyokato@cpan.org>
2007-01-09 16:12:29 +0000

    Import of LYOKATO/Pod-ProjectDocs-0.30 from CPAN.
    gitpan-cpan-distribution: Pod-ProjectDocs
    gitpan-cpan-version:      0.30
    gitpan-cpan-path:         LYOKATO/Pod-ProjectDocs-0.30.tar.gz
    gitpan-cpan-author:       LYOKATO
    gitpan-cpan-maturity:     released

--- lib/Pod/ProjectDocs.pm
+++ lib/Pod/ProjectDocs.pm
@@ -16,7 +16,7 @@ use Pod::ProjectDocs::IndexPage;
 
 __PACKAGE__->mk_accessors(qw/managers components config/);
 
-our $VERSION = '0.26';
+our $VERSION = '0.30';
 
 sub new {
     my $class = shift;
